9f7ed9770e5fc57443a9450d0ffe87dbd1472462
joaoezx <joao.a.freitas0405@gmail.com>
2024-02-05 00:58:51 -0300

    Soma todos os valores das despesas

--- JS.js
+++ JS.js
@@ -7,9 +7,20 @@ function AdicionarDespesa(){
 
 }
 
+var valores = [] 
+
 function AdicionarValor() {
     
     var valor = parseFloat(document.getElementById("Valor2").value)
+    valores.push(valor)
     document.getElementById("valorLista").innerHTML += "<li>" + "R$" + valor + "</li>"
 }
 
+function conta(){
+    let soma = 0;
+    for(var a = 0; a < valores.length; a++) {
+    soma += valores[a]
+    }
+
+    document.getElementById("resultado").innerHTML = `Total de Despesas: R$${soma.toFixed(2)}`
+}
